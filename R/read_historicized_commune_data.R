bf6b4004276de89cec1688c4fd28d6d41679b4f9
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-02-17 13:33:18 +0100

    on.exit instead of tryCatch

--- R/read_historicized_commune_data.R
+++ R/read_historicized_commune_data.R
@@ -7,107 +7,102 @@
 #' @export
 #' @importFrom logging logdebug
 read_historicized_commune_data <- function() {
+  RECORD_HIST_URL <- 'http://www.bfs.admin.ch/bfs/portal/de/index/infothek/nomenklaturen/blank/blank/gem_liste/02.Document.96666.zip'
+  zip.file.name <- tempfile(fileext='.zip')
+  logdebug(zip.file.name)
+  on.exit(unlink(zip.file.name), add=TRUE)
+
+  download.file(RECORD_HIST_URL, zip.file.name)
+  
+  unzip.dir.name <- tempfile()
+  logdebug(unzip.dir.name)
   tryCatch(
     expr={
-      RECORD_HIST_URL <- 'http://www.bfs.admin.ch/bfs/portal/de/index/infothek/nomenklaturen/blank/blank/gem_liste/02.Document.96666.zip'
-      zip.file.name <- tempfile(fileext='.zip')
-      logdebug(zip.file.name)
-      download.file(RECORD_HIST_URL, zip.file.name)
+      file.list <- unzip(zip.file.name, list=T)
+      unzip(zip.file.name, exdir=unzip.dir.name)
       
-      unzip.dir.name <- tempfile()
-      logdebug(unzip.dir.name)
-      tryCatch(
-        expr={
-          file.list <- unzip(zip.file.name, list=T)
-          unzip(zip.file.name, exdir=unzip.dir.name)
-          
-          # Reading using unz() and recoding "on the fly"
-          # didn't work for some reason
-          ft <- list(
-            canton=list(n='KT', colnames=c(
-              'cId',
-              'cAbbreviation',
-              'cLongName',
-              'cDateOfChange')),
-            district=list(n='BEZ', colnames=c(
-              'dHistId',
-              'cId',
-              'dId',
-              'dLongName',
-              'dShortName',
-              'dEntryMode',
-              'dAdmissionNumber',
-              'dAdmissionMode',
-              'dAdmissionDate',
-              'dAbolitionNumber',
-              'dAbolitionMode',
-              'dAbolitionDate',
-              'dDateOfChange')),
-            municipality=list(n='GDE', colnames=c(
-              'mHistId',
-              'dHistId',
-              'cAbbreviation',
-              'mId',
-              'mLongName',
-              'mShortName',
-              'mEntryMode',
-              'mStatus',
-              'mAdmissionNumber',
-              'mAdmissionMode',
-              'mAdmissionDate',
-              'mAbolitionNumber',
-              'mAbolitionMode',
-              'mAbolitionDate',
-              'mDateOfChange'))
-          )
-          codes <- c(`11`='Municipality',
-                     `12`='Area not allocated to mun.',
-                     `13`='Cantonal part of lake',
-                     `15`='District',
-                     `16`='Canton',
-                     `17`='Area not allocated to distr.',
-                     `20`='First-time registration',
-                     `21`='Creation',
-                     `22`='Change of name (d)',
-                     `23`='Change of name (m)',
-                     `24`='Reassignment to other d/c',
-                     `26`='Change of area',
-                     `27`='Formal renumbering',
-                     `29`='Abolition',
-                     `30`='Mutation canceled')
-          
-          l <- lapply(X=ft, FUN=function(t) {
-            logdebug('Parsing data set: %s', t$n)
-            
-            fname <- grep(paste0('_', t$n, '.txt'), file.list$Name, value=T)
-            fpath <- file.path(unzip.dir.name, fname)
-            dat <- read.table(fpath, sep='\t', quote='', col.names=t$colnames, fileEncoding='ISO8859-15', stringsAsFactors=F)
-            
-            date.names <- grep('Date', names(dat), value=T)
-            logdebug('Date names: %s', date.names)
-            for (n in date.names) dat[, n] <- as.Date(dat[, n], format='%d.%m.%Y')
-            
-            mode.names <- grep('Mode', names(dat), value=T)
-            logdebug('Mode names: %s', mode.names)
-            for (n in mode.names) dat[, n] <- factor(dat[, n], levels=names(codes), labels=codes)
-            
-            numeric.names <- grep('Id|Number', names(dat), value=T)
-            logdebug('Numeric names: %s', numeric.names)
-            for (n in numeric.names) dat[, n] <- as.numeric(dat[, n])
-            
-            # Last column is compulsory
-            stopifnot(!is.na(dat[, tail(t$colnames, 1)]))
-            
-            dat
-          })
-        },
-        finally={
-          unlink(unzip.dir.name, recursive=T)
-        }
+      # Reading using unz() and recoding "on the fly"
+      # didn't work for some reason
+      ft <- list(
+        canton=list(n='KT', colnames=c(
+          'cId',
+          'cAbbreviation',
+          'cLongName',
+          'cDateOfChange')),
+        district=list(n='BEZ', colnames=c(
+          'dHistId',
+          'cId',
+          'dId',
+          'dLongName',
+          'dShortName',
+          'dEntryMode',
+          'dAdmissionNumber',
+          'dAdmissionMode',
+          'dAdmissionDate',
+          'dAbolitionNumber',
+          'dAbolitionMode',
+          'dAbolitionDate',
+          'dDateOfChange')),
+        municipality=list(n='GDE', colnames=c(
+          'mHistId',
+          'dHistId',
+          'cAbbreviation',
+          'mId',
+          'mLongName',
+          'mShortName',
+          'mEntryMode',
+          'mStatus',
+          'mAdmissionNumber',
+          'mAdmissionMode',
+          'mAdmissionDate',
+          'mAbolitionNumber',
+          'mAbolitionMode',
+          'mAbolitionDate',
+          'mDateOfChange'))
       )
+      codes <- c(`11`='Municipality',
+                 `12`='Area not allocated to mun.',
+                 `13`='Cantonal part of lake',
+                 `15`='District',
+                 `16`='Canton',
+                 `17`='Area not allocated to distr.',
+                 `20`='First-time registration',
+                 `21`='Creation',
+                 `22`='Change of name (d)',
+                 `23`='Change of name (m)',
+                 `24`='Reassignment to other d/c',
+                 `26`='Change of area',
+                 `27`='Formal renumbering',
+                 `29`='Abolition',
+                 `30`='Mutation canceled')
+      
+      l <- lapply(X=ft, FUN=function(t) {
+        logdebug('Parsing data set: %s', t$n)
+        
+        fname <- grep(paste0('_', t$n, '.txt'), file.list$Name, value=T)
+        fpath <- file.path(unzip.dir.name, fname)
+        dat <- read.table(fpath, sep='\t', quote='', col.names=t$colnames, fileEncoding='ISO8859-15', stringsAsFactors=F)
+        
+        date.names <- grep('Date', names(dat), value=T)
+        logdebug('Date names: %s', date.names)
+        for (n in date.names) dat[, n] <- as.Date(dat[, n], format='%d.%m.%Y')
+        
+        mode.names <- grep('Mode', names(dat), value=T)
+        logdebug('Mode names: %s', mode.names)
+        for (n in mode.names) dat[, n] <- factor(dat[, n], levels=names(codes), labels=codes)
+        
+        numeric.names <- grep('Id|Number', names(dat), value=T)
+        logdebug('Numeric names: %s', numeric.names)
+        for (n in numeric.names) dat[, n] <- as.numeric(dat[, n])
+        
+        # Last column is compulsory
+        stopifnot(!is.na(dat[, tail(t$colnames, 1)]))
+        
+        dat
+      })
     },
     finally={
-      unlink(zip.file.name)
+      unlink(unzip.dir.name, recursive=T)
     }
   )
 }

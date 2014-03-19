c179567ee81a392a45e8f233e43378b84a0d0d81
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-19 11:14:18 +0100

    fix: use evalWithMemoization instead of addMemoization to have more control over the interface

--- R/swcGetData.R
+++ R/swcGetData.R
@@ -1,6 +1,6 @@
 #' Import historic commune data from the web
 #' 
-#' This functon returns historic commune.  If necessary,
+#' This functon returns historic commune data.  If necessary,
 #' \code{\link{swcReadData}} is called.  The data is cached
 #' with the help of the \link{R.cache} package.
 #' 
@@ -9,11 +9,11 @@
 #' 
 #' @param force If \code{TRUE}, reload data from the web.
 #' 
-#' @usage swcGetData(force = FALSE)
-#' 
 #' @seealso \code{\link{swcReadData}}
 #' 
 #' @export
-#' @importFrom R.cache addMemoization
+#' @importFrom R.cache evalWithMemoization
 #' @include swcReadData.R
-swcGetData <- R.cache::addMemoization(swcReadData)
+swcGetData <- function(force=FALSE) {
+  R.cache::evalWithMemoization(swcReadData(), force=force)
+}

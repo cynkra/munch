38f4aa3c6f0b843a761a2edbca28e5751c456b84
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-25 00:32:03 +0100

    recreate SwissBirths dataset

---
+++ orig-data/SwissBirths.R
@@ -0,0 +1,17 @@
+library(pxR)
+vz <- read.px("orig-data/px-d-01-2D02.px")
+vzd <- as.data.frame(vz)
+vzds <- subset(vzd, Geschlecht.des.Kindes == "Total" & Staatsangehörigkeit.des.Kindes == "Total" & Altersklasse.der.Mutter == "Total")
+vzdm <- subset(vzds, grepl('^[.][.][.][.][.][.]([0-9]+).*$', Kanton.......Gemeinde.........))
+vzdp <- transform(vzdm,
+                  MunicipalityID=as.numeric(gsub('^[.][.][.][.][.][.]([0-9]+) (.*)$', '\\1', Kanton.......Gemeinde.........)),
+                  MunicipalityName=gsub('^[.][.][.][.][.][.]([0-9]+) (.*)$', '\\2', Kanton.......Gemeinde.........))
+summary(vzdp)
+SwissBirths <- with(vzdp, data.frame(Year=Jahr, MunicipalityID=kimisc::ofactor(MunicipalityID),
+                                   MunicipalityName=kimisc::ofactor(MunicipalityName),
+                                   Births=value))
+with(SwissBirths, stopifnot(as.numeric(MunicipalityID) == as.numeric(MunicipalityName)))
+SwissBirths <- plyr::arrange(SwissBirths, Year, MunicipalityID)
+save(SwissBirths, file="data/SwissBirths.rda", compress="xz")
+summary(SwissBirths)
+str(SwissBirths)

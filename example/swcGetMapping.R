671ce37a71136913efdc568d6c0129d52bebf0f8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-25 01:10:40 +0100

    complete example

--- example/swcGetMapping.R
+++ example/swcGetMapping.R
@@ -1,32 +1,22 @@
-logging::basicConfig("INFO")
-
 data(SwissPop)
-mutIDPop <- plyr::dlply(
-  SwissPop,
-  c(), #"Jahr",
-  function(piece) {
-    SwissCommunes:::getMostProbableMutationId(swc=swcGetData(), as.character(piece$MunicipalityID))
-  }
-)
-
-(mutIDPop <- unique(unlist(mutIDPop)))
-
 data(SwissBirths)
-mutIDBirths <- plyr::dlply(
-  SwissBirths,
-  c(), #"Year",
-  function(piece)
-    SwissCommunes:::getMostProbableMutationId(swc=swcGetData(), municipalityIds=piece$MunicipalityID),
-  .progress="text"
-)
 
-mutIDBirths <- unique(unlist(mutIDBirths))
-
-
+# Show mismatch of municipality IDs:
 ids.from <- with(SwissPop, MunicipalityID)
 ids.to <- with(SwissBirths, MunicipalityID)
 setdiff(ids.from, ids.to)
 setdiff(ids.to, ids.from)
+
+# Compute mapping and count non-matching municipality IDs:
 mapping <- swcGetMapping(ids.from=ids.from, ids.to=ids.to)
-with(mapping, sum(mapping$mId.from != mapping$mId.to))
+with(mapping, sum(mapping$mIdAsNumber.from != mapping$mIdAsNumber.to))
+
+# These should be looked at in some detail:
 subset(mapping, !(MatchType %in% c("valid", "missing")))
+
+# Test for injectivity. The result shows that the mapping is almost injective,
+# only one "from" commune is mapped to more than one other "to" commune.
+# This situation requires further examination.
+valid.mapping <- subset(mapping, MatchType == "valid")
+valid.mapping.dupes <- subset(valid.mapping, duplicated(mId.from))
+(noninjective.mapping <- subset(valid.mapping, mId.from %in% valid.mapping.dupes$mId.from))

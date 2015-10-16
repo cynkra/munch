8e65d7e0c509a2e3fafac82d0635001d56d593ed
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-17 00:31:57 +0200

    postpone conversion to factor

--- R/swcGetMapping.R
+++ R/swcGetMapping.R
@@ -60,11 +60,6 @@ swcGetMapping <- function(swc=swcGetData(), ids.from, ids.to) {
   resultTable <- function(histId, inId, name) {
     ret <- swc$municipality[match(histId, swc$municipality$mHistId),
                                  c('mHistId', 'cAbbreviation', 'mId', 'mLongName', 'mShortName')]
-    if (is.factor(inId)) {
-      ret$mIdAsNumber <- ret$mId
-      ret$mId <- factor(ret$mId, levels=levels(inId))
-    }
-
     ret$MatchType <- mt(ifelse(ret$mId %in% inId, "valid", "missing"))
     names(ret) <- cn(names(ret), name)
     ret
@@ -79,12 +74,26 @@ swcGetMapping <- function(swc=swcGetData(), ids.from, ids.to) {
     ret
   }
 
+  fixFactor <- function(ret, inId, name) {
+    nameId <- paste0("mId.", name)
+    nameIdAsNumber <- paste0("mIdAsNumber.", name)
+    if (is.factor(inId)) {
+      ret[[nameIdAsNumber]] <- ret[[nameId]]
+      ret[[nameId]] <- factor(ret[[nameId]], levels=levels(inId))
+    }
+
+    ret
+  }
+
   ret.from <- resultTable(ret$from, ids.from.int, "from")
   ret.to <- resultTable(ret$to, ids.to.int, "to")
   ret <- cbind(ret.from, ret.to)
   ret <- plyr::rbind.fill(ret,
                           extraTable(ret.from$mId.from, ids.from.int, "from"),
                           extraTable(ret.to$mId.to, ids.to.int, "to"))
+  ret <- fixFactor(ret, ids.from, "from")
+  ret <- fixFactor(ret, ids.to, "to")
+
   dMatchType <- c(
     `valid.valid`="valid",
     `missing.missing`="missing",

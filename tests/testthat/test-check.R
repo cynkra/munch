56cbc5fa088de28143fd1cfa8e1f9006ce046e87
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-16 13:18:47 +0200

    don't load plyr

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -3,6 +3,6 @@ context("check")
 test_that("swcCheckData loads data as advertised", {
   expect_that(check <- swcCheckData(), not(throws_error()))
   expect_equal(names(check), c("statesWithNonUniqueAdmissionNumbers", "stateSequencesWithDecreasingDate"))
-  expect_true(all(unlist(llply(check, is.data.frame))))
-  expect_true(all(unlist(llply(check, nrow)) > 0))
+  expect_true(all(unlist(plyr::llply(check, is.data.frame))))
+  expect_true(all(unlist(plyr::llply(check, nrow)) > 0))
 })
--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -3,6 +3,6 @@ context("get")
 test_that("swcGetData loads data as advertised", {
   swc <- swcGetData()
   expect_equal(names(swc), c("canton", "district", "municipality"))
-  expect_true(all(unlist(llply(swc, is.data.frame))))
-  expect_true(all(unlist(llply(swc, nrow)) > 0))
+  expect_true(all(unlist(plyr::llply(swc, is.data.frame))))
+  expect_true(all(unlist(plyr::llply(swc, nrow)) > 0))
 })

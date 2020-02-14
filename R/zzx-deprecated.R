412939a9401156e550e3bf448e343108143ec6d5
Tobias <tobias@cynkra.com>
2020-02-14 15:20:46 +0100

    internal

--- R/zzx-deprecated.R
+++ R/zzx-deprecated.R
@@ -1,7 +1,9 @@
 #' @export
+#' @keywords internal
 #' @rdname deprecated
 swcGetMapping <- new_fun_forward(swc_get_mapping, old_fwd_name = "swcGetMapping")
 
 #' @export
+#' @keywords internal
 #' @rdname deprecated
 swcGetMutations <- new_fun_forward(swc_get_mutations, old_fwd_name = "swcGetMutations")

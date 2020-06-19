728767edfc8afe1c1b93d38cedc815556199b4ad
Kirill Müller <krlmlr+r@mailbox.org>
2020-06-19 16:09:55 +0200

    Import

--- R/import.R
+++ R/import.R
@@ -1,10 +1,10 @@
 #' @import rlang
 #' @importFrom utils head
-#' @importFrom purrr map map_chr discard map2_dfr map_dfr map2 map_lgl
+#' @importFrom purrr map map_chr discard map2_dfr map_dfr map2 map_lgl accumulate
 #' @import dplyr
 #' @import tibble
 #' @rawNamespace import(glue, except = c(collapse))
-#' @importFrom tidyr unnest
+#' @importFrom tidyr unnest nest
 #' @rawNamespace import(lubridate, except = c(intersect, union, setdiff))
 #' @importFrom stats na.omit
 NULL

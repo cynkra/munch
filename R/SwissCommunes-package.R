6a3eb74f1c2a0362970a4191cdc300da5c5a7ede
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-19 06:11:31 +0100

    Integrate

--- R/SwissCommunes-package.R
+++ R/SwissCommunes-package.R
@@ -69,3 +69,20 @@ NULL
 #' @docType data
 #' @usage SwissPop
 NULL
+
+#' @importFrom tibble tibble
+#' @importFrom utils download.file read.table tail unzip
+#' @examples
+#' cantons
+#' district_mutations
+#' municipality_mutations
+"_PACKAGE"
+
+#' Canton data
+"cantons"
+
+#' District mutation data
+"district_mutations"
+
+#' Municipality mutation data
+"municipality_mutations"

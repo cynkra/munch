929c305578e6665c6b5622f3058a1da1c4d111b5
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-19 07:54:06 +0100

    Move

--- R/helper-read.R
+++ R/helper-read.R
@@ -118,9 +118,9 @@ swc_read_data <- function() {
 overwrite_data <- function() {
   data <- swc_read_data()
 
-  readr::write_csv(data$canton, csv_file("canton"))
-  readr::write_csv(data$district, csv_file("district_mutations"))
-  readr::write_csv(data$municipality, csv_file("municipality_mutations"))
+  readr::write_csv(data$canton, csv_file("mut/canton"))
+  readr::write_csv(data$district, csv_file("mut/district_mutations"))
+  readr::write_csv(data$municipality, csv_file("mut/municipality_mutations"))
 }
 
 #' check if new data is identical to old data

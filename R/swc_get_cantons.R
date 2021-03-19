fa023a05cbcaad45b0877ab288ec84a9cf6cc971
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-19 07:12:45 +0100

    - Write compact and flat tables to main branch.

--- R/swc_get_cantons.R
+++ R/swc_get_cantons.R
@@ -5,3 +5,7 @@ swc_get_cantons <- function() {
 csv_file <- function(name) {
   system.file("csv", paste0(name, ".csv"), package = "SwissCommunes")
 }
+
+new_csv_file <- function(name) {
+  file.path("inst/csv", paste0(name, ".csv"))
+}
---
+++ R/write_tables.R
@@ -0,0 +1,20 @@
+write_all_mapping_tables <- function() {
+  SOURCE_YEAR <- 2005
+
+  years <- seq(SOURCE_YEAR, lubridate::year(Sys.Date()), by = 1)
+
+  compact <- map(years, write_mapping_table, source_year = SOURCE_YEAR, type = "compact")
+  flat <- map(years, write_mapping_table, source_year = SOURCE_YEAR, type = "flat")
+
+  invisible()
+}
+
+write_mapping_table <- function(source_year, year, type) {
+  mapping <- swc_get_merger_mapping_table(source_year, year, type = type)
+
+  path <- new_csv_file(file.path(type, year))
+  dir.create(dirname(path), showWarnings = FALSE)
+  readr::write_csv(mapping, path)
+
+  tibble(year, type, path, nrow = nrow(mapping))
+}

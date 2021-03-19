68bdfd10360e63b19a930c13c58c16d37abeb684
Kirill Müller <krlmlr+r@mailbox.org>
2021-03-19 08:54:06 +0100

    Try, erors in write_all_mapping_tables()

--- R/get_merger_mapping_table.R
+++ R/get_merger_mapping_table.R
@@ -54,7 +54,7 @@ swc_get_merger_mapping_table <- function(start_year, end_year, canton = NULL, ty
   mutations_base <-
     mutations %>%
     select(mAdmissionDate, mMutationNumber, mId.x, mShortName.x, mId.y, mShortName.y) %>%
-    mutate(year = as.integer(lubridate::year(mAdmissionDate))) %>%
+    mutate(year = as.integer(lubridate::year(mAdmissionDate - 1)) + 1L) %>%
     select(-mAdmissionDate, -mMutationNumber) %>%
     distinct()
 

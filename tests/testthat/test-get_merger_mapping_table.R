a172e02a2fa92d5601bcd9420f6fdfc824929da2
Tobias <tobias@cynkra.com>
2021-03-30 16:54:22 +0200

    tweak tests

--- tests/testthat/test-get_merger_mapping_table.R
+++ tests/testthat/test-get_merger_mapping_table.R
@@ -5,14 +5,14 @@ test_that("swc_get_merger_mapping_table() works", {
       filter(mun_id_x != mun_id_y),
     tibble::tribble(
       ~year, ~mun_id_x, ~mun_id_y, ~short_name_x, ~short_name_y,
-      2018,      1022,      1030,      "Altwis",   "Hitzkirch",
-      2018,      1126,      1123,  "Ebersecken",  "Altishofen",
-      2018,      1130,      1151,     "Gettnau",    "Willisau",
-      2019,      1022,      1030,      "Altwis",   "Hitzkirch",
-      2019,      1126,      1123,  "Ebersecken",  "Altishofen",
-      2019,      1130,      1151,     "Gettnau",    "Willisau",
-      2020,      1022,      1030,      "Altwis",   "Hitzkirch",
-      2020,      1130,      1151,     "Gettnau",    "Willisau"
+      2018,      1022L,     1030L,      "Altwis",   "Hitzkirch",
+      2018,      1126L,     1123L,  "Ebersecken",  "Altishofen",
+      2018,      1130L,     1151L,     "Gettnau",    "Willisau",
+      2019,      1022L,     1030L,      "Altwis",   "Hitzkirch",
+      2019,      1126L,     1123L,  "Ebersecken",  "Altishofen",
+      2019,      1130L,     1151L,     "Gettnau",    "Willisau",
+      2020,      1022L,     1030L,      "Altwis",   "Hitzkirch",
+      2020,      1130L,     1151L,     "Gettnau",    "Willisau"
     )
   )
 
@@ -21,9 +21,9 @@ test_that("swc_get_merger_mapping_table() works", {
       filter(mun_id_x != mun_id_y),
     tibble::tribble(
       ~year_from, ~year_to, ~mun_id_x, ~short_name_x, ~mun_id_y, ~short_name_y,
-      2018,     2020,      1022,      "Altwis",      1030,   "Hitzkirch",
-      2018,     2019,      1126,  "Ebersecken",      1123,  "Altishofen",
-      2018,     2020,      1130,     "Gettnau",      1151,    "Willisau"
+      2018,     2020,      1022L,      "Altwis",      1030L,   "Hitzkirch",
+      2018,     2019,      1126L,  "Ebersecken",      1123L,  "Altishofen",
+      2018,     2020,      1130L,     "Gettnau",      1151L,    "Willisau"
     )
   )
 
@@ -32,19 +32,19 @@ test_that("swc_get_merger_mapping_table() works", {
       filter(mun_id_x != mun_id_y),
     tibble::tribble(
       ~year, ~mun_id_x, ~mun_id_y,         ~short_name_x, ~short_name_y,
-      2019,       873,       889,      "Kirchenthurnen",     "Thurnen",
-      2019,       874,       889,           "Lohnstorf",     "Thurnen",
-      2019,       876,       889,        "Mühlethurnen",     "Thurnen",
-      2019,       937,       939,        "Schwendibach", "Steffisburg",
-      2019,       996,       981,          "Wolfisberg",  "Niederbipp",
-      2019,      1126,      1123,          "Ebersecken",  "Altishofen",
-      2019,      2111,      2117, "Villaz-Saint-Pierre",      "Villaz",
-      2019,      2116,      2117,          "La Folliaz",      "Villaz",
-      2019,      2185,      2237,            "Corserey",        "Prez",
-      2019,      2213,      2237,              "Noréaz",        "Prez",
-      2019,      2221,      2237,    "Prez-vers-Noréaz",        "Prez",
-      2019,      3926,      3901,            "Maladers",        "Chur",
-      2019,      4114,      4095,      "Schinznach-Bad",       "Brugg"
+      2019,       873L,       889L,      "Kirchenthurnen",     "Thurnen",
+      2019,       874L,       889L,           "Lohnstorf",     "Thurnen",
+      2019,       876L,       889L,        "Mühlethurnen",     "Thurnen",
+      2019,       937L,       939L,        "Schwendibach", "Steffisburg",
+      2019,       996L,       981L,          "Wolfisberg",  "Niederbipp",
+      2019,      1126L,      1123L,          "Ebersecken",  "Altishofen",
+      2019,      2111L,      2117L, "Villaz-Saint-Pierre",      "Villaz",
+      2019,      2116L,      2117L,          "La Folliaz",      "Villaz",
+      2019,      2185L,      2237L,            "Corserey",        "Prez",
+      2019,      2213L,      2237L,              "Noréaz",        "Prez",
+      2019,      2221L,      2237L,    "Prez-vers-Noréaz",        "Prez",
+      2019,      3926L,      3901L,            "Maladers",        "Chur",
+      2019,      4114L,      4095L,      "Schinznach-Bad",       "Brugg"
     )
   )
 
@@ -53,19 +53,19 @@ test_that("swc_get_merger_mapping_table() works", {
       filter(mun_id_x != mun_id_y),
     tibble::tribble(
       ~year_from, ~year_to, ~mun_id_x,         ~short_name_x, ~mun_id_y, ~short_name_y,
-      2019,     2019,       873,      "Kirchenthurnen",       889,     "Thurnen",
-      2019,     2019,       874,           "Lohnstorf",       889,     "Thurnen",
-      2019,     2019,       876,        "Mühlethurnen",       889,     "Thurnen",
-      2019,     2019,       937,        "Schwendibach",       939, "Steffisburg",
-      2019,     2019,       996,          "Wolfisberg",       981,  "Niederbipp",
-      2019,     2019,      1126,          "Ebersecken",      1123,  "Altishofen",
-      2019,     2019,      2111, "Villaz-Saint-Pierre",      2117,      "Villaz",
-      2019,     2019,      2116,          "La Folliaz",      2117,      "Villaz",
-      2019,     2019,      2185,            "Corserey",      2237,        "Prez",
-      2019,     2019,      2213,              "Noréaz",      2237,        "Prez",
-      2019,     2019,      2221,    "Prez-vers-Noréaz",      2237,        "Prez",
-      2019,     2019,      3926,            "Maladers",      3901,        "Chur",
-      2019,     2019,      4114,      "Schinznach-Bad",      4095,       "Brugg"
+      2019,     2019,       873L,      "Kirchenthurnen",       889L,     "Thurnen",
+      2019,     2019,       874L,           "Lohnstorf",       889L,     "Thurnen",
+      2019,     2019,       876L,        "Mühlethurnen",       889L,     "Thurnen",
+      2019,     2019,       937L,        "Schwendibach",       939L, "Steffisburg",
+      2019,     2019,       996L,          "Wolfisberg",       981L,  "Niederbipp",
+      2019,     2019,      1126L,          "Ebersecken",      1123L,  "Altishofen",
+      2019,     2019,      2111L, "Villaz-Saint-Pierre",      2117L,      "Villaz",
+      2019,     2019,      2116L,          "La Folliaz",      2117L,      "Villaz",
+      2019,     2019,      2185L,            "Corserey",      2237L,        "Prez",
+      2019,     2019,      2213L,              "Noréaz",      2237L,        "Prez",
+      2019,     2019,      2221L,    "Prez-vers-Noréaz",      2237L,        "Prez",
+      2019,     2019,      3926L,            "Maladers",      3901L,        "Chur",
+      2019,     2019,      4114L,      "Schinznach-Bad",      4095L,       "Brugg"
     )
   )
 

43cecbe771fd4b04d274493764f2d6d67d40ad69
Kirill Müller <krlmlr+r@mailbox.org>
2021-11-30 14:33:57 +0100

    Read columns as strings

--- R/swc_get_municipality_mutations.R
+++ R/swc_get_municipality_mutations.R
@@ -1,3 +1,3 @@
 swc_get_municipality_mutations <- function() {
-  readr::read_csv(csv_file("mut/municipality_mutations"), col_types = "iiciccfiifDifDD")
+  readr::read_csv(csv_file("mut/municipality_mutations"), col_types = "iiciccciicDicDD")
 }

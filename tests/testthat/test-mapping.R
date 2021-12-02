3a51c845a9109a2ab190919f7e08b8e21b736b8f
Tobias Schieferdecker <tobias@cynkra.com>
2021-12-02 15:36:48 +0100

    use testthat 3 and replace some old practices

--- tests/testthat/test-mapping.R
+++ tests/testthat/test-mapping.R
@@ -1,13 +1,11 @@
-context("mapping")
-
 test_that("mapping returns integer columns", {
   mapping <- swc_get_mapping(ids_from = 500, ids_to = 2875)
 
-  expect_is(mapping$mHistId.from, "integer")
-  expect_is(mapping$mId.from, "integer")
+  expect_type(mapping$mHistId.from, "integer")
+  expect_type(mapping$mId.from, "integer")
   expect_null(mapping$mIdAsNumber.from)
-  expect_is(mapping$mHistId.to, "integer")
-  expect_is(mapping$mId.to, "integer")
+  expect_type(mapping$mHistId.to, "integer")
+  expect_type(mapping$mId.to, "integer")
   expect_null(mapping$mIdAsNumber.to)
 })
 
@@ -17,12 +15,12 @@ test_that("mapping returns factors for factor input", {
     ids_to = factor(2875)
   )
 
-  expect_is(mapping$mHistId.from, "integer")
-  expect_is(mapping$mId.from, "factor")
-  expect_is(mapping$mIdAsNumber.from, "integer")
-  expect_is(mapping$mHistId.to, "integer")
-  expect_is(mapping$mId.to, "factor")
-  expect_is(mapping$mIdAsNumber.to, "integer")
+  expect_type(mapping$mHistId.from, "integer")
+  expect_true(class(mapping$mId.from) == "factor")
+  expect_type(mapping$mIdAsNumber.from, "integer")
+  expect_type(mapping$mHistId.to, "integer")
+  expect_true(class(mapping$mId.to) == "factor")
+  expect_type(mapping$mIdAsNumber.to, "integer")
 })
 
 test_that("deprecation error", {
